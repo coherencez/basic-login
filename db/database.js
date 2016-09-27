468c0c75079d2eedf9d9349c6b6cb3f8cfaf587b
Thomas Swann <tkswann@mac.com>
2016-09-27 18:41:27 -0500

    removed OR locahost operator from mongoDB env var

--- db/database.js
+++ db/database.js
@@ -1,6 +1,6 @@
 'use strict'
 const  mongoose = require('mongoose')
-  ,   MONGODB_URL = process.env.MOGODB_URL || 'mongodb://localhost:27017/basic-login'
+  ,   MONGODB_URL = process.env.MOGODB_URL
 
 mongoose.Promise = Promise
 
